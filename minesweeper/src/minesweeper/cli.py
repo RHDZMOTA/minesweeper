c8144325bc8a3c9095b7cda8ecf88c084cbe9881
rhdzmota <rhdzmota@gmail.com>
2021-08-21 02:41:28 -0500

    START Add initial implementation

---
+++ minesweeper/src/minesweeper/cli.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+from .board import Grid
+from .solver import Solver
+
+DEFAULT_GRID = """
+[3] [?] [2] [?]
+[?] [?] [ ] [?]
+"""
+
+
+class CLI:
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--timeout", type=int, default=100)
+        parser.add_argument("--grid", type=str, default=DEFAULT_GRID)
+        self.args = parser.parse_args()
+        self.grid = Grid.from_string(self.args.grid)
+        self.solver_instance = Solver(grid=self.grid)
+
+    def run(self):
+        self.solver_instance.run(timeout=self.args.timeout)
+        if self.solver_instance.finished():
+            self.solver_instance.final_state.show()
